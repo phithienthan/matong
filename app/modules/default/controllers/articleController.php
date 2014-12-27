e9679878809685bb9e7e97f691ba4dbb24d8a5be
phithienthan <phithienthan1@gmail.com>
2014-12-27 16:55:10 +0700

    add other news and title style

--- app/modules/default/controllers/articleController.php
+++ app/modules/default/controllers/articleController.php
@@ -20,14 +20,18 @@ Class articleController Extends defaultController
         $article = $articleModel->getArticleInfo($articleId, 1);
         $categoryId = $article['category_id'];
         $categoryInfo = $categoryModel->getCategoryInfo($categoryId);
-        /*         * * set a template variable ** */
+        // Get other article
+        $top = 11;
+        $articles = $articleModel->getAllArticlesByCategoryId($categoryId, 1, $top);                
+        $this->view->data['articles'] = $articles;        
+        // Set a template variable
         $this->view->data['categoryInfo'] = $categoryInfo;
         $this->view->data['article'] = $article;
 
         $this->view->setTitle($article["title"]);
         $this->view->setDescription($article["title"]);
         $this->view->setKeywords($article["title"]);
-        /*         * * load the index template ** */
+        // Load the index template
         $this->view->show('view_detail');
     }
 
@@ -43,14 +47,14 @@ Class articleController Extends defaultController
         }
         $pageSize = 20;
         $articles = $articleModel->getAllArticlesByCategoryId($categoryId, $page, $pageSize);
-        /*         * * set a template variable ** */
+        // Set a template variable
         $this->view->data['categoryInfo'] = $categoryInfo;
         $this->view->data['articles'] = $articles;
 
         $this->view->setTitle($categoryInfo["title"]);
         $this->view->setDescription($categoryInfo["title"]);
         $this->view->setKeywords($categoryInfo["title"]);
-        /*         * * load the index template ** */
+        // Load the index template
         $this->view->show('list');
     }
 
